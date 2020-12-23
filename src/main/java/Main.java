61221dc93429f9064283dec8132bfc5936dd5882
maciej-123 <maciejzajaczkowskiscience@gmail.com>
2020-12-23 18:04:08 +0000

    v

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,9 +11,12 @@ public class Main {
     public static void main(String[] args) {
         //GET_Requests g = new GET_Requests("https://phabservlet1.herokuapp.com/text_database");
 
-        GET_Requests g2 = new GET_Requests("https://phabservlet1.herokuapp.com/create_test_database");
-        GET_Requests g3 = new GET_Requests("https://phabservlet1.herokuapp.com/return_test_database");
+        //GET_Requests g2 = new GET_Requests("https://phabservlet1.herokuapp.com/create_test_database");
+        //GET_Requests g3 = new GET_Requests("https://phabservlet1.herokuapp.com/return_phab_paddington");
 
+        String a = "tabs@tabs";
+
+        POST_Requests p = new POST_Requests(a,"https://phabservlet1.herokuapp.com/_decreaseStock");
 
         //System.out.println(g.returnText());
         //database_on_local_host_test dbtest = new database_on_local_host_test();
